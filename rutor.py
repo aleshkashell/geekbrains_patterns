cba50531338aeae728075f765e3e672954076301
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2020-02-04 18:17:10 +0300

    Create searcher for rutor

--- rutor.py
+++ rutor.py
@@ -1,16 +1,19 @@
 import asyncio
 import aiohttp
 from aiohttp import client_exceptions
-import logging
 from lxml import html
 from interfaces import TorrentInterface
+from logger import Log
 
 
 class Rutor(TorrentInterface):
     def __init__(self):
-        self.log = logging.getLogger(__name__)
+        self.log = Log(__name__)
         self.session = aiohttp.ClientSession()
         self._init_variables()
+
+    def _init_logging(self):
+        pass
 
     def _init_variables(self):
         self._rutor = dict()
@@ -24,11 +27,12 @@ class Rutor(TorrentInterface):
         try:
             async with self.session.get(url, allow_redirects=False) as resp:
                 if resp.status != 200:
+                    self.log.info(f"Got code {resp.status} from {url}")
                     await asyncio.sleep(2.0)
                     return None
-                print(resp.status)
                 return await resp.text()
-        except client_exceptions.ClientConnectionError:
+        except client_exceptions.ClientConnectionError as e:
+            self.log.info(e)
             await asyncio.sleep(2.0)
             return None
 
@@ -40,7 +44,7 @@ class Rutor(TorrentInterface):
         return links
 
     @staticmethod
-    def parse(self, html_text):
+    def parse(html_text):
         tree = html.fromstring(html_text)
         elements = tree.xpath('//table[@width]//tr')
         results = list()
@@ -68,9 +72,13 @@ class Rutor(TorrentInterface):
 
     async def search(self, search_str):
         futures = [self.fetch_url(link) for link in self._generate_links(search_str)]
-        print('\n'.join(self._generate_links(search_str)))
+        self.log.debug(f"Generated links: {'  '.join(self._generate_links(search_str))}")
         done, pending = await asyncio.wait(futures, return_when=asyncio.FIRST_COMPLETED)
         for future in pending:
             future.cancel()
-        return done.pop().result()
-
+        try:
+            print(f'Done: {done}')
+            html_page = done.pop().result()
+        except:
+            return None
+        return self.parse(html_page)
